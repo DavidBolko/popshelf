f89f8e896b64bbcbe21b209e28b88c510b01f390
DavidBolko <bolko.dev@outlook.com>
2025-06-07 15:14:02 +0200

    finalna uprava

---
+++ app/src/main/java/com/Secrets.kt
@@ -0,0 +1,9 @@
+package com
+
+class Secrets{
+    companion object{
+        const val idgb_id = "jfds7r6nhpge7w012dpct2wxzijq6px"
+        const val idgb_scrt = "zjo6y2irjzv903ugr5clk6spygteksx"
+        const val imdb = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTViYjY2OWI5OTM4MTA1MmMxNTE3NTdkMDMzNGYxMiIsIm5iZiI6MTc0NTg1OTg5Ny4yODcwMDAyLCJzdWIiOiI2ODBmYjUzOTVhOWI3OGQyNzI4MTY5NDkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.twVsTa--1FSnzgigMDEVpfOUyNLCqKWFNMKbbq6Th0Qx"
+    }
+}
